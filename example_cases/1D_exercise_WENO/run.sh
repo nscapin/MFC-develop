baafba8773308be94a16f55827f126c895c084cc
sbryngelson <sbryngelson@gmail.com>
2021-06-18 01:54:21 -0400

    asdf

--- example_cases/1D_exercise_WENO/run.sh
+++ example_cases/1D_exercise_WENO/run.sh
@@ -16,12 +16,37 @@
 # export NV_ACC_TIME=1
 # export NV_ACC_DEBUG=1
 
+# modules for hpctoolkit
+export HPCTOOLKIT_MODULES_HPCTOOLKIT=""
+
+# environment settings for this example
+export HPCTOOLKIT_LULESH_ACC_MODULES_BUILD=""
+export HPCTOOLKIT_LULESH_ACC_CXX="pgc++ -DUSE_MPI=0 -DSEDOV_SYNC_POS_VEL_LATE"
+export HPCTOOLKIT_LULESH_ACC_ACCFLAGS="-acc -Minfo=accel -fast -gopt"
+export HPCTOOLKIT_LULESH_ACC_SUBMIT="bsub $HPCTOOLKIT_PROJECTID -W 20 -nnodes 1 $HPCTOOLKIT_RESERVATION"
+export HPCTOOLKIT_LULESH_ACC_RUN="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-run -o log.run.out -e log.run.error"
+export HPCTOOLKIT_LULESH_ACC_RUN_PC="$HPCTOOLKIT_LULESH_ACC_SUBMIT -J lulesh-run-pc -o log.run-pc.out -e log.run-pc.error"
+export HPCTOOLKIT_LULESH_ACC_BUILD="sh"
+export HPCTOOLKIT_LULESH_ACC_LAUNCH="jsrun -n 1 -g 1 -a 1 --smpiargs off"
+
+  # set flag for this example
+
+BINARY=simulation
+EXEC=../../src/simulation_code/${BINARY}
+OUT=hpctoolkit-${BINARY}-acc 
+STRUCT_FILE=$BINARY.hpcstruct
 
 rm -r hpctoolkit*
-jsrun -r1 -a7 -c7 -g1 ../../src/pre_process_code/pre_process
-jsrun -r1 -a7 -c7 -g1 hpcrun -t ../../src/simulation_code/simulation
-hpcstruct ../../src/simulation_code/simulation
-jsrun -r1 -a1 -c1 -g1 hpcprof -S simulation.hpcstruct -I ../../src/simulation_code/+ hpctoolkit-simulation-measurements-*
+rm -rf ${OUT}.m ${OUT}.d $STRUCT_FILE
+
+jsrun -r1 -a1 -c1 -g1 ../../src/pre_process_code/pre_process
+${HPCTOOLKIT_LULESH_ACC_LAUNCH} hpcrun -o $OUT.m -e REALTIME -e gpu=nvidia -t ${EXEC}
+hpcstruct -o $STRUCT_FILE ${EXEC}
+hpcstruct --gpucfg no $OUT.m
+hpcprof -S $STRUCT_FILE -o $OUT.d $OUT.m
+
+# jsrun -r1 -a1 -c1 -g1 hpcprof -S simulation.hpcstruct -I ../../src/simulation_code/+ hpctoolkit-simulation-measurements-*
+
 
 # jsrun -r1 -a7 -c7 -g1 ../../src/pre_process_code/pre_process
 # jsrun -r1 -a7 -c7 -g1 ../../src/simulation_code/simulation
