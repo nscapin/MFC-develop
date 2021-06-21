6b88ad166173326788830c7fd5499079c14e153f
Spencer Bryngelson <sbryngelson@gmail.com>
2021-06-20 23:51:03 -0400

    add a line to start the MPS server on compute node

--- example_cases/1D_exercise_WENO/run.sh
+++ example_cases/1D_exercise_WENO/run.sh
@@ -11,6 +11,9 @@
 ## 42/6 = 7 where 42 = number of total cores (c)
 ## (a == c) usually
 
+nvidia-cuda-mps-control  -d
+nvidia-cuda-mps-server
+
 # export PGI_ACC_NOTIFY=2
 # export NV_ACC_NOTIFY=3
 # export NV_ACC_TIME=1
@@ -49,10 +52,16 @@
 
 # jsrun -r1 -a1 -c1 -g1 nsys profile -o output-sys.prof --stats=true -t openacc,nvtx  --force-overwrite true ../../src/simulation_code/simulation
 
-mpirun -n 1 ../../src/pre_process_code/pre_process
-mpirun -n 1 ../../src/simulation_code/simulation
+# mpirun -n 1 ../../src/pre_process_code/pre_process
+# mpirun -n 1 ../../src/simulation_code/simulation
 
-# mpirun -n 4 nsys profile -o output-sys.prof_%q{OMPI_COMM_WORLD_RANK} --stats=true -t openacc,nvtx  --force-overwrite true ../../src/simulation_code/simulation
+mpirun -n 2 ../../src/pre_process_code/pre_process
+# mpirun -n 2 ../../src/simulation_code/simulation
+
+# mpirun -n 4 ../../src/pre_process_code/pre_process
+# mpirun -n 4 ../../src/simulation_code/simulation
+
+mpirun -n 2 nsys profile -o output-sys.prof_%q{OMPI_COMM_WORLD_RANK} --stats=true -t openacc,nvtx  --force-overwrite true ../../src/simulation_code/simulation
 # mpirun -n 1 nsys profile -o output-sys.prof --stats=true -t openacc,nvtx  --force-overwrite true ../../src/simulation_code/simulation
 
 
